24b56f7fce0e7f1adcb4f8a70c22638167cd8d3a
IsaacDMD <isaacdjimadjo830@gmail.com>
2026-07-03 15:48:36 +0000

    ajout du json contenant les quiz

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,26 @@
 <template>
-  
+  <div>
+    {{ quiz }}
+  </div>
 
 </template>
 
 <script setup>
+import { onMounted, ref } from 'vue';
+
+const quiz = ref(null)
+
+onMounted(() => {
+  fetch('/quiz.json')
+  .then(r => {
+    if(r.ok){
+      return r.json()
+    }
+    throw new Error ('Impossible de charger le json')
+  })
+  .then(data => {
+    quiz.value = data
+  })
+})
 
 </script>
